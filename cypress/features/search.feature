fb636c7ad53baa484a647a028926ee6ee86b3d60
L0L4-chan <gaiafwfg@gmail.com>
2024-11-12 12:14:01 +0100

    search

--- cypress/features/search.feature
+++ cypress/features/search.feature
@@ -3,10 +3,10 @@ Feature: Hotel room under some preferences
 
     Background: The user should be loog in
         Given: User is logged in
-    Scenario: Simple search
+    Scenario: Complete search
         When: I introduce the correct information
         Then: I should see a list of options
 
-    Scenario: Simple search2
-        When: I introduce the correct information
-        Then: I should see a list of options
+    Scenario: Search without a data
+        When: I introduce the part information
+        Then: I should see a warning
--- cypress/support/steps/searchsteps.js
+++ cypress/support/steps/searchsteps.js
@@ -26,3 +26,19 @@ Then("I should see a list of options", ()=>{
 cy.get("#__next > div > div > main > div:nth-child(3) > div > div.c-section___1kLue > div").should('be visible'); 
 
 });
+
+
+When("I introduce the part information",()=>{ 
+    cy.get("#__next > div > div > div.search___2t_BT > div > div > div:nth-child(5) > form > div.b-wrapper___3CcRc > div > div.calendar___3o0XN > div > div.wrapper___1qplQ > button > div")
+    .find("#__next > div > div > div.search___2t_BT > div > div > div:nth-child(5) > form > div.b-wrapper___3CcRc > div > div.calendar___3o0XN > div > div.content___NTabH > div > div > div > div > div > div > div > div.react-calendar__viewContainer > div:nth-child(1) > div > div > div.react-calendar__month-view__days > button:nth-child(34) > abbr")
+    .click();
+    cy.find("#__next > div > div > div.search___2t_BT > div > div > div:nth-child(5) > form > div.b-wrapper___3CcRc > div > div.calendar___3o0XN > div > div.content___NTabH > div > div > div > div > div > div > div > div.react-calendar__viewContainer > div:nth-child(2) > div > div > div.react-calendar__month-view__days > button:nth-child(36)").click();
+    cy.get("#__next > div > div > div.search___2t_BT > div > div > div:nth-child(5) > form > div.submit___3burc > button").click()
+    
+    });
+    
+    Then("I should see a warning", ()=>{
+    
+    cy.get("#__next > div > div > div.search___2t_BT > div > div > div:nth-child(5) > form > div.b-wrapper___3CcRc > div > div.destination___YiVIM > div > div.wrapper___1qplQ > div.notification-wrapper___31yg- > div > div > div > span").should('be visible'); 
+    
+    });
