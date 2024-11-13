90e21311eca9cd0b9d170bf316f6cd0f69301a8d
L0L4-chan <gaiafwfg@gmail.com>
2024-11-13 08:18:54 +0100

    Cambios en configuración

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+
 module.exports = defineConfig({
   e2e: {
-    baseUrl : "https://www.melia.com/es",
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
--- cypress/features/reviewroom.feature
+++ cypress/features/reviewroom.feature
@@ -2,7 +2,7 @@ Feature: Review room information
 
     Background: A search have occurred
         Given: User is logged in 
-        And I introduce the correct information
+        Then: I introduce the correct information
 
     Scenario: Review room
         When: I select a room from the list
