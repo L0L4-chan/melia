d675fe182badd2c7ff570374a2017b787d3bca49
L0L4-chan <gaiafwfg@gmail.com>
2024-11-14 10:18:06 +0100

    wait

--- cypress/support/const/constLogin.js
+++ cypress/support/const/constLogin.js
@@ -1,13 +1,14 @@
 export class ConstLogin{
+    wait = 1000;
     elementsLogin  = {
 
-        userName : () => cy.customFindElements( "#user", 100),
-        userPassword : () => cy.customFindElements("#password", 100),
-        submit:  () => cy.customFindElements("#submitBtn", 100),
-        iconLogUSer : () => cy.customFindElements("#button_content_2cPJr" ,100),
-        error : () => cy.customFindElements("#error_3B9Tw", 100),
-        missingpassword : () => cy.customFindElements("#user-request-step > div.content___3JH08 > div > div.form-nodes___Ox0HI > div.c-form___EX68s > form > div.b-grid___1vXmM > div > div:nth-child(4) > div > div.errors___IhYnC > p", 100),
-        missingname : () => cy.customFindElements("#user-request-step > div.content___3JH08 > div > div.form-nodes___Ox0HI > div.c-form___EX68s > form > div.b-grid___1vXmM > div > div:nth-child(1) > div > div.errors___IhYnC > p", 100),
+        userName : () => cy.customFindElements( "#user", wait),
+        userPassword : () => cy.customFindElements("#password", wait),
+        submit:  () => cy.customFindElements("#submitBtn", wait),
+        iconLogUSer : () => cy.customFindElements("#button_content_2cPJr" ,wait),
+        error : () => cy.customFindElements("#error_3B9Tw", wait),
+        missingpassword : () => cy.customFindElements("#user-request-step > div.content___3JH08 > div > div.form-nodes___Ox0HI > div.c-form___EX68s > form > div.b-grid___1vXmM > div > div:nth-child(4) > div > div.errors___IhYnC > p", wait),
+        missingname : () => cy.customFindElements("#user-request-step > div.content___3JH08 > div > div.form-nodes___Ox0HI > div.c-form___EX68s > form > div.b-grid___1vXmM > div > div:nth-child(1) > div > div.errors___IhYnC > p", wait),
     };
 
 
